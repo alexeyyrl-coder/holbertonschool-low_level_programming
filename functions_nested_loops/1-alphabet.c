b7d195b6817265298f225f37a38146b91918e17b
alexeyyrl-coder <alexeyyrl@email.com>
2026-06-29 14:19:24 +0200

    add all good thing

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints the lowercase alphabet
+ */
+
+void print_alphabet(void)
+{
+	
+	char alp;
+	
+	alp = 'a';
+	while (alp <= 'z')
+	{
+		_putchar(alp);
+		alp++;
+	}
+	_putchar('\n');
+}
