aca3f4fef3d2d4ea569bba57324684914a26f227
alexeyyrl-coder <alexeyyrl@email.com>
2026-06-29 14:42:34 +0200

    add all good thing

---
+++ functions_nested_loops/2-print_alphabet_10x.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10- Prints the lowercase alphabet
+ */
+
+void print_alphabet_x10(void)
+{
+
+        char alp;
+	int count;
+
+        alp = 'a';
+	count = 0;
+
+	while (count <= 9)
+	{
+        while (alp <= 'z')
+        {
+                _putchar(alp);
+                alp++;
+        }
+	alp = 'a';
+	count++;
+	_putchar('\n');
+	}
+}
+~
