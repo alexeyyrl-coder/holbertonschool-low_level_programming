77b4426f11615cee51d0af7a3a3787edceea9671
alexeyyrl-coder <alexeyyrl@email.com>
2026-06-30 02:33:04 +0200

    add all good thing

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolut valeur
+ * @n: the integer to compute
+ *
+ * Returne: the absolute value of n
+ */
+int _abs(int n)
+{
+	
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+}
