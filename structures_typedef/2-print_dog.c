ed13e267118068d202c1e4d8c0bff76cfd9eaf8b
alexeyyrl-coder <alexeyyrl@email.com>
2026-07-07 01:01:30 +0200

    add all good thing

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - print a variable of type struct dog
+ * @d: Pointer to the dog structure
+ */
+void print_dog(struct dog *d)
+{
+
+        if (d == 0)
+        {
+                return;
+        }
+
+	if (d->name == 0)
+	{
+        	printf("Name: (nil)\n");
+	}
+	else
+	{
+		printf("Name: %s\n", d->name);
+	}
+
+	printf("Age: %f\n", d->age);
+
+	if (d->owner == 0)
+	{
+		printf("Owner: (nil)\n");
+	}
+	else
+	{
+		printf("Owner: %s\n", d->owner);
+	}
+
+}
