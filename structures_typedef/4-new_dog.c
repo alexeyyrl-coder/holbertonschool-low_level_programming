0bb1fee20fe3df519c92cf601fa10e41baadca27
alexeyyrl-coder <alexeyyrl@email.com>
2026-07-07 14:01:42 +0200

    add all good thing

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,5 +1,5 @@
-#include "dog.h"
 #include <stdlib.h>
+#include "dog.h"
 
 /**
  * _strlen - Returns the length of a string
@@ -11,11 +11,11 @@ int _strlen(char *s)
 {
 	int i;
 
-        i = 0;
-        while (s[i] != '\0')
-        {
-                i++;
-        }
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
 
 	return (i);
 }
@@ -35,44 +35,40 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	dog = malloc(sizeof(dog_t));
 	if (dog == 0)
-	{	
+	{
 		return (0);
 	}
 
-	/* malloc pour dog->name ici */
-	dog->name = malloc(sizeof(char) * (_strlen(name) +1));
+	dog->name = malloc(sizeof(char) * (_strlen(name) + 1));
 	if (dog->name == 0)
 	{
 		free(dog);
 		return (0);
 	}
 
-	/* malloc pour dog->owner ici */
-	dog->owner = malloc(sizeof(char) * (_strlen(owner)+ 1));
+	dog->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
 	if (dog->owner == 0)
 	{
 		free(dog->name);
 		free(dog);
 		return (0);
 	}
-	
-	dog->age = age;
-	
+
 	i = 0;
-	while (owner[i] != '\0')
-	{
-	 	dog->owner[i] = owner[i];
-		i++;
-	}
-	dog->owner[i] = '\0';
-	
-	i = 0;
-	while (name[i] != '\0' )
+	while (name[i] != '\0')
 	{
 		dog->name[i] = name[i];
 		i++;
 	}
 	dog->name[i] = '\0';
+
+	i = 0;
+	while (owner[i] != '\0')
+	{
+		dog->owner[i] = owner[i];
+		i++;
+	}
+	dog->owner[i] = '\0';
 
 	dog->age = age;
 
