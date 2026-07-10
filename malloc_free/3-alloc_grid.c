db752fb00816067efd104bac656a0a1d383b3085
alexeyyrl-coder <alexeyyrl@email.com>
2026-07-10 12:34:28 +0200

    add all good thing

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -2,6 +2,42 @@
 #include "main.h"
 
 /**
+ * free_lines - frees allocated lines of a grid
+ * @grid: grid to free
+ * @height: number of lines to free
+ */
+static void free_lines(int **grid, int height)
+{
+	int i;
+
+	i = 0;
+	while (i < height)
+	{
+		free(grid[i]);
+		i++;
+	}
+
+	free(grid);
+}
+
+/**
+ * fill_line - fills a line with zeros
+ * @line: line to fill
+ * @width: width of the line
+ */
+static void fill_line(int *line, int width)
+{
+	int w;
+
+	w = 0;
+	while (w < width)
+	{
+		line[w] = 0;
+		w++;
+	}
+}
+
+/**
  * alloc_grid - returns a pointer to a 2 dimensional array of integers
  * @width: width of the grid
  * @height: height of the grid
@@ -12,19 +48,13 @@ int **alloc_grid(int width, int height)
 {
 	int **grid;
 	int h;
-	int w;
-	int i;
-	
-	if(width <= 0 || height <= 0)
-	{
+
+	if (width <= 0 || height <= 0)
 		return (NULL);
-	}
 
 	grid = malloc(sizeof(int *) * height);
-	if(grid == NULL)
-	{
+	if (grid == NULL)
 		return (NULL);
-	}
 
 	h = 0;
 	while (h < height)
@@ -32,23 +62,12 @@ int **alloc_grid(int width, int height)
 		grid[h] = malloc(sizeof(int) * width);
 		if (grid[h] == NULL)
 		{
-			i = 0;
-			while (i < h)
-			{
-				free(grid[i]);
-				i++;
-			}
-			free(grid);
+			free_lines(grid, h);
 			return (NULL);
 		}
-
-		w = 0;
-		while (w < width)
-		{
-			grid[h][w] = 0;
-			w++;
-		}
+		fill_line(grid[h], width);
 		h++;
 	}
+
 	return (grid);
 }
