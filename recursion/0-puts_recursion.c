f3a35f162ee40b0df7a1b22829048c2ca7901b7d
alexeyyrl-coder <alexeyyrl@email.com>
2026-07-10 18:11:57 +0200

    add all good thing

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -6,7 +6,6 @@
  */
 void _puts_recursion(char *s)
 {
-	_putchar(*s);
 	if (*s == '\0')
 	{
 	_putchar('\n');
@@ -14,7 +13,7 @@ void _puts_recursion(char *s)
 	}
 	else
 	{
-	_puts_recursion(s + 1);
 	_putchar(*s);
+	_puts_recursion(s + 1);
 	}
 }
