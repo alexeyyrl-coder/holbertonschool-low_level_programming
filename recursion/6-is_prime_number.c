1b19a9f6d25d180ae976c858829ae82e6f4d8d4d
alexeyyrl-coder <alexeyyrl@email.com>
2026-07-11 01:12:00 +0200

    add all good thing

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -9,16 +9,16 @@
  */
 int _prime_helper(int n, int i)
 {
-	if(i == n)
+	if (i == n)
 	{
 		return (1);
 	}
 
-	if(n % i == 0)
+	if (n % i == 0)
 	{
 		return (0);
 	}
-	return(_prime_helper(n, i + 1));
+	return (_prime_helper(n, i + 1));
 
 }
 /**
@@ -29,9 +29,9 @@ int _prime_helper(int n, int i)
  */
 int is_prime_number(int n)
 {
-	if(n <= 1)
+	if (n <= 1)
 	{
 		return (0);
 	}
-	return(_prime_helper(n, 2));
+	return (_prime_helper(n, 2));
 }
