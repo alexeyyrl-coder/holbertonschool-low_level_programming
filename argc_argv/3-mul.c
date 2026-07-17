8534e08492b80660236f091a5d08254a6e6d4674
alexeyyrl-coder <alexeyyrl@email.com>
2026-07-17 10:42:23 +0200

    add all good thing

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies two numbers
+ * @argc: number of command-line arguments
+ * @argv: array of command-line arguments
+ *
+ * Return: 0 on success, 1 on error
+ */
+int main(int argc, char *argv[])
+{
+        int first;
+        int second;
+        int result;
+
+        if (argc != 3)
+        {
+                printf("Error\n");
+                return (1);
+        }
+	
+	first = atoi(argv[1]);
+	second = atoi(argv[2]);
+
+	result = first * second;
+
+        printf("%d\n", result);
+        return (0);
+}
