04c9f4d44d84be42f8b40477f4d07189c86b5a3c
alexeyyrl-coder <alexeyyrl@email.com>
2026-07-17 10:45:39 +0200

    add all good thing

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -10,21 +10,22 @@
  */
 int main(int argc, char *argv[])
 {
-        int first;
-        int second;
-        int result;
+	int first;
+	int second;
+	int result;
 
-        if (argc != 3)
-        {
-                printf("Error\n");
-                return (1);
-        }
-	
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
 	first = atoi(argv[1]);
 	second = atoi(argv[2]);
 
 	result = first * second;
 
-        printf("%d\n", result);
-        return (0);
+	printf("%d\n", result);
+
+	return (0);
 }
