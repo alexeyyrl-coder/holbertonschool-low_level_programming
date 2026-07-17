e818b44f61969bee15ec79697a05447be6977afd
alexeyyrl-coder <alexeyyrl@email.com>
2026-07-18 01:10:01 +0200

    add all good thing

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -3,6 +3,45 @@
 #include "variadic_functions.h"
 
 /**
+ * print_value - prints one argument according to its type
+ * @type: type of the argument
+ * @arguments: variadic argument list
+ * @separator: string printed before valid arguments
+ *
+ * Return: Nothing.
+ */
+static void print_value(char type, va_list *arguments,
+			const char **separator)
+{
+	char *string;
+
+	switch (type)
+	{
+	case 'c':
+		printf("%s%c", *separator, va_arg(*arguments, int));
+		*separator = ", ";
+		break;
+	case 'i':
+		printf("%s%d", *separator, va_arg(*arguments, int));
+		*separator = ", ";
+		break;
+	case 'f':
+		printf("%s%f", *separator, va_arg(*arguments, double));
+		*separator = ", ";
+		break;
+	case 's':
+		string = va_arg(*arguments, char *);
+		if (string == NULL)
+			string = "(nil)";
+		printf("%s%s", *separator, string);
+		*separator = ", ";
+		break;
+	default:
+		break;
+	}
+}
+
+/**
  * print_all - prints arguments according to a format
  * @format: list of argument types
  *
@@ -13,10 +52,8 @@ void print_all(const char * const format, ...)
 	va_list arguments;
 	unsigned int i;
 	const char *separator;
-	char *string;
 
 	va_start(arguments, format);
-
 	i = 0;
 	separator = "";
 
@@ -24,36 +61,7 @@ void print_all(const char * const format, ...)
 	{
 		while (format[i] != '\0')
 		{
-			switch (format[i])
-			{
-			case 'c':
-				printf("%s%c", separator,
-				       va_arg(arguments, int));
-				separator = ", ";
-				break;
-			case 'i':
-				printf("%s%d", separator,
-				       va_arg(arguments, int));
-				separator = ", ";
-				break;
-			case 'f':
-				printf("%s%f", separator,
-				       va_arg(arguments, double));
-				separator = ", ";
-				break;
-			case 's':
-				string = va_arg(arguments, char *);
-
-				if (string == NULL)
-					string = "(nil)";
-
-				printf("%s%s", separator, string);
-				separator = ", ";
-				break;
-			default:
-				break;
-			}
-
+			print_value(format[i], &arguments, &separator);
 			i++;
 		}
 	}
